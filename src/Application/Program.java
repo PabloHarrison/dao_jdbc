bf5a1a2d70fbc9c2de08512187f59f3e06058090
PabloHarrison <pabloharrison.77@gmail.com>
2025-11-05 10:35:45 -0300

    SellerDaoJDBC and DaoFactory

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,16 +1,19 @@
 package Application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 public class Program {
     public static void main(String[] args){
 
         Department obj = new Department(1, "books");
         Seller seller = new Seller(3, "Maria", "maria@gmail.com", LocalDate.of(1990, 5, 12), 1000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }
