8180e3186f68511d47e35bc3fa5614c2141c8dd6
PabloHarrison <pabloharrison.77@gmail.com>
2025-11-05 10:23:05 -0300

    Seller entity class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,12 +1,17 @@
 package Application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Program {
     public static void main(String[] args){
 
         Department obj = new Department(1, "books");
+        Seller seller = new Seller(3, "Maria", "maria@gmail.com", LocalDate.of(1990, 5, 12), 1000.0, obj);
 
-        System.out.println(obj);
+        System.out.println(seller);
     }
 }
