3370adc9f2a5cf4ccfefca6a96cd4ef5e61f0b0b
PabloHarrison <pabloharrison.77@gmail.com>
2025-11-06 10:09:55 -0300

    update implementation

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -36,5 +36,11 @@ public class Program {
         Seller seller2 = new Seller(null, "Greg", "greg@gmail.com", LocalDate.now(), 4000.0, department);
         sellerDao.insert(seller2);
         System.out.println("Inserted new Id = " + seller2.getId());
+
+        System.out.println("\n=== Test 5: Update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update Completed!");
     }
 }
